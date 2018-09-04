2efd3a5e4564ed9990a06d98045f9caf5790dde1
Javier Fernandez <jfernandez@igalia.com>
2018-09-04 14:09:38 -0700

    [css-grid] Performance optimizations in the Baseline alignment code
    Since we integrated the baseline alignment logic in the grid tracks
    sizing algorithm, its impact on performance has grown considerably.
    
    The analysis of the new logic added and its overhead, due to different
    operations, shows that evaluating the item's participation in the
    baseline alignment context is the most expensive one. It's specially
    demanding the evaluation of the grid item's alignment properties.
    
    Considering that currently we are doing this for every grid item, this
    CL propose to reuse the loop we already have to clear the grid item's
    override size to cache the items with a baseline value in their
    alignment CSS properties.
    
    Thanks to this cache we can determine the item's participation in a
    baseline alignment context in the different phases of the track sizing
    algorithm, with almost no cost (compared to the current logic).
    
    Additionally, this cache is also used in the alignment phase of the
    grid layout logic; this change helps to avoid the various issues we
    have been suffering related to the different evaluations of the item's
    participation in baseline during the different phases of the grid
    layout algorithm.
    
    BUG = 873452
    
    Change-Id: Ida27be11ae0f5c455e6077367a277981ab35cec1

--- css/css-grid/alignment/support/style-change.js
+++ css/css-grid/alignment/support/style-change.js
@@ -3,3 +3,13 @@ function evaluateStyleChange(element, phase, expectedProperty, expectedResult) {
     element.setAttribute(expectedProperty, expectedResult);
     checkLayout("." + phase, false);
 }
+function evaluateStyleChange1(phase, expectedResult) {
+    for (var item in expectedResult) {
+        var element = document.getElementById(item);
+        element.className += " " + phase;
+        for (var key in expectedResult[item]) {
+            element.setAttribute(key, expectedResult[item][key]);
+        }
+    }
+    checkLayout("." + phase, false);
+}
